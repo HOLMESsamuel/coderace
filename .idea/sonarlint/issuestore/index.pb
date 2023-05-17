68bbec219dcb66615c2367c382743f287cd525ce
Samuel Holmes <sam.holmes@live.fr>
2023-05-17 08:50:09 +0200

    add splashscreen and improve style

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+k
+;src-tauri/src/folder_manager/folder_writer/folder_writer.rs,0\f\0fcadb986b065fe6f3059a2411e74dd92ccc08ff
+S
+#src-tauri/src/folder_manager/mod.rs,8\6\862f779b1b204410571c68ad9905632c443ae6e3
+c
+3src-tauri/src/folder_manager/folder_archiver/mod.rs,9\b\9b611e0ff1f21195ee8e1f8502606a73cd6f5df4
+o
+?src-tauri/src/folder_manager/folder_archiver/folder_archiver.rs,9\a\9aeb924e4f9d42d92d62eb62f0bff82c4e140cc3
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,7 +11,7 @@ mod race;
 use std::fs::File;
 use std::path::Path;
 use crate::race::race;
-use tauri::{CustomMenuItem, Menu, Submenu};
+use tauri::{CustomMenuItem, Menu, Submenu, Manager};
 use folder_manager::folder_writer::folder_creator::folder_creator;
 use folder_manager::folder_archiver::folder_archiver;
 use tauri::api::dialog;
@@ -38,6 +38,22 @@ fn main() {
           }
           _ => {}
         }
+      })
+      .setup(|app| {
+        let splashscreen_window = app.get_window("splashscreen").unwrap();
+        let main_window = app.get_window("main").unwrap();
+        // we perform the initialization code on a new task so the app doesn't freeze
+        tauri::async_runtime::spawn(async move {
+          // initialize your app here instead of sleeping :)
+          println!("Initializing...");
+          std::thread::sleep(std::time::Duration::from_secs(2));
+          println!("Done initializing.");
+
+          // After it's done, close the splashscreen and display the main window
+          splashscreen_window.close().unwrap();
+          main_window.show().unwrap();
+        });
+        Ok(())
       })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
